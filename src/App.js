7314bd17e4395c962e9eac7e55983871ae06532b
Rachel Moore <moorer@gcsnc.com>
2021-01-27 09:41:57 -0500

    table data showing with css

--- src/App.js
+++ src/App.js
@@ -1,70 +1,15 @@
-import React, {Component} from "react";
-import "./styles.css";
-import * as ReactBootStrap from "react-bootstrap";
+import React from 'react';
 
-class App extends Component {
+import './App.css'
+import { BasicTable } from './components/BasicTable'
 
-constructor(props) {
-  super(props)
-  this.state = {
-    items:[],
-    loading: false
-  }
+function App() {
+    return (
+        <div className='App'>
+            <BasicTable />
+        </div>
+    )
 }
 
-componentDidMount() {
-  fetch("https://randomuser.me/api/?results=20")
-  .then((response) => response.json())
-  .then((response) => {
-    this.setState({
-      items:response.results,
-      loading:true
-    })
-  })
-}
-
-render() {
-    const {items,loading} = this.state
-
-    if(!loading) {
-      return (
-        <div>Loading...</div>
-      )
-    }
-    else{
-    // return (
-      let container= <div className="container">
-
-      {items.map(item => (
-        
-        <tr key={item.md5}>
-          <td><h4>{item.name.first} {item.name.last}</h4></td>
-          <td><h4> {item.gender}</h4></td>
-          <td><h4>{item.dob.age}</h4></td>
-      </tr>
-      
-      ))}
-      </div>
-    // )
-}
-        return (
-          <div className="App">
-            {container}
-        <ReactBootStrap.Table striped bordered hover>
-        <thead>
-          <tr>
-            <th scope="col">First Name</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Age</th>
-            <th scope="col">Gender</th>
-          </tr>
-        </thead>
-        </ReactBootStrap.Table>
-          </div>
-        )
-        }
-
-      }
-        
-      
 export default App;
+
---
+++ src/components/columns.js
@@ -0,0 +1,23 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Name',
+        accessor: 'name'
+    },
+    {
+        Header: 'Email',
+        accessor: 'email'
+    },
+    {
+        Header: 'Gender',
+        accessor: 'gender'
+    },
+    {
+        Header: 'Age',
+        accessor: 'age'
+    }
+
+]
